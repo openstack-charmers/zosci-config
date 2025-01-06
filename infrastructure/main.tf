be9dc6dc3c41eae03ea6b79446b296a03b609c99
Felipe Reyes <felipe.reyes@canonical.com>
2025-01-06 13:45:57 -0300

    Add an ingress for zuul-web

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1447,45 +1447,45 @@ resource "kubernetes_deployment" "zuul_preview" {
     }
 }
 
-# resource "kubernetes_ingress_v1" "zuul_web_ingress" {
-#     wait_for_load_balancer = true
-#     metadata {
-#         name = "zuul-web-ingress"
-#         namespace = kubernetes_namespace.zuul.metadata[0].name
-#     }
+resource "kubernetes_ingress_v1" "zuul_web_ingress" {
+    wait_for_load_balancer = true
+    metadata {
+        name = "zuul-web-ingress"
+        namespace = kubernetes_namespace.zuul.metadata[0].name
+    }
 
-#     spec {
-#         default_backend {
-#             service {
-#                 name = kubernetes_service.zuul_web.metadata.0.name
-#                 port {
-#                     number = kubernetes_service.zuul_web.spec.0.port.0.port
-#                 }
-#             }
-#         }
-#         ingress_class_name = var.ingress_class_name
-#         rule {
-#             http {
-#                 path {
-#                     backend {
-#                         service {
-#                             name = kubernetes_service.zuul_web.metadata.0.name
-#                             port {
-#                                 number = kubernetes_service.zuul_web.spec.0.port.0.port
-#                             }
-#                         }
-#                     }
-#                     path = "/"
-#                 }
-#             }
-#         }
-#     }
-# }
+    spec {
+        default_backend {
+            service {
+                name = kubernetes_service.zuul_web.metadata.0.name
+                port {
+                    number = kubernetes_service.zuul_web.spec.0.port.0.port
+                }
+            }
+        }
+        ingress_class_name = var.ingress_class_name
+        rule {
+            http {
+                path {
+                    backend {
+                        service {
+                            name = kubernetes_service.zuul_web.metadata.0.name
+                            port {
+                                number = kubernetes_service.zuul_web.spec.0.port.0.port
+                            }
+                        }
+                    }
+                    path = "/"
+                }
+            }
+        }
+    }
+}
 
-# output "load_balancer_hostname" {
-#     value = kubernetes_ingress_v1.zuul_web_ingress.status.0.load_balancer.0.ingress.0.hostname
-# }
+output "load_balancer_hostname" {
+    value = kubernetes_ingress_v1.zuul_web_ingress.status.0.load_balancer.0.ingress.0.hostname
+}
 
-# output "load_balancer_ip" {
-#     value = kubernetes_ingress_v1.zuul_web_ingress.status.0.load_balancer.0.ingress.0.ip
-# }
+output "load_balancer_ip" {
+    value = kubernetes_ingress_v1.zuul_web_ingress.status.0.load_balancer.0.ingress.0.ip
+}
